d6892402a7c6504bfec990a4e4294c810efc8670
oluwatobiloba <oluwateezzy03@gmail.com>
2023-09-29 05:33:54 +0100

    update

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ const redisClient = require('./utils/redis');
 
 (async () => {
     console.log(redisClient.isAlive());
-    console.log(await redisClient.get('myKey'));
-    await redisClient.set('myKey', 12, 5);
-    console.log(await redisClient.get('myKey'));
+    console.log(await redisClient.get('my'));
+    await redisClient.set('my', 12, 5);
+    console.log(await redisClient.get('my'));
 
     setTimeout(async () => {
-        console.log(await redisClient.get('myKey'));
+        console.log(await redisClient.get('my'));
     }, 1000*10)
 })();
--- utils/redis.js
+++ utils/redis.js
@@ -1,15 +1,38 @@
 const redis = require('redis');
+const {promisify} = require('util')
 
 class RegisClient {
   constructor() {
-    const client = redis.createClient();
-    client.on('error', (err) => {
-      console.error(`Redis client not connected to the server: ${err.message}`);
+    this.client = redis.createClient();
+    this.isConnected = true
+    this.client.on('error', (err) => {
+      console.error(`Redis client not connected to the server: ${err.message}`)
+      this.isConnected = false
     });
+    this.client.on('connect', () => {
+        this.isConnected = true
+    })
+    this.getAsyn = promisify(this.client.get).bind(this.client)
+    this.delAsyn = promisify(this.client.del).bind(this.client)
+    this.setAsyn = promisify(this.client.setex).bind(this.client)
+
+  }
+  isAlive(){
+    return this.isConnected
   }
 
-  isAlive() {
+  async get(key){
+    return await this.getAsyn(key)
+  }
 
+  async set(key, value, time){
+    return this.setAsyn(key, time, value)
+  }
+
+  async del(key) {
+    return await this.delAsyn(key)
   }
 }
-module.exports = RegisClient;
+
+const regisClient = new RegisClient()
+module.exports = regisClient
